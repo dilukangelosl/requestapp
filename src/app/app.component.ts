fcfae6b9c9e10cef204a258831ee8c3b9eb35ab9
Diluk Angelo <dilukangelo@gmail.com>
2017-07-31 16:51:40 +0530

    fm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+userid :any;
+message:any;
+
+constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/requests', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+
+    this.user.subscribe(res => {
+     if(res){
+       this.userid = res.uid;
+     }
+        })
+
+
+
+
+}
+
+
+
+login() {
+    this.afAuth.auth.signInAnonymously();
+  
+}
+
+logout() {
+    
+    this.afAuth.auth.signOut();
+}
+
+Send(desc: string) {
+
+  var d = new Date().toISOString();
+  if(this.userid){
+  this.items.push({ request: desc, userid:this.userid, resdate:d}).then((res) => {
+    console.log(res);
+  })
+    this.msgVal = '';
+  }else{
+    alert("Please login");
+  }
+    
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule } from '@angular/forms';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+ apiKey: "AIzaSyC0jUOYHFXi9UCufS2jZBDkMG4tWLT6iP4",
+    authDomain: "shaheed-ab25a.firebaseapp.com",
+    databaseURL: "https://shaheed-ab25a.firebaseio.com",
+    projectId: "shaheed-ab25a",
+    storageBucket: "",
+    messagingSenderId: "459382011520"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+     AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
